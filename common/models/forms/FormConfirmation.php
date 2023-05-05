a532f129167131a4d958bfee16f5ba68ecefb59a
Gulomjon Sulaymonov <uzpsychologist@gmail.com>
2023-05-05 22:39:09 +0500

    user id fixed

--- common/models/forms/FormConfirmation.php
+++ common/models/forms/FormConfirmation.php
@@ -8,6 +8,7 @@ use common\models\ApplicationFormMedia;
 use common\models\ApplicationWizard;
 use common\models\User;
 use common\models\UserApplications;
+use frontend\models\ImaUsers;
 use Yii;
 
 /**
@@ -43,7 +44,7 @@ class FormConfirmation extends \yii\db\ActiveRecord
             [['user_id', 'user_application_id', 'user_application_wizard_id', 'is_confirmed'], 'integer'],
             [['confirmed_date'], 'safe'],
             [['user_application_id'], 'exist', 'skipOnError' => true, 'targetClass' => UserApplications::class, 'targetAttribute' => ['user_application_id' => 'id']],
-            [['user_id'], 'exist', 'skipOnError' => true, 'targetClass' => User::class, 'targetAttribute' => ['user_id' => 'id']],
+            [['user_id'], 'exist', 'skipOnError' => true, 'targetClass' => ImaUsers::class, 'targetAttribute' => ['user_id' => 'id']],
             [['user_application_wizard_id'], 'exist', 'skipOnError' => true, 'targetClass' => ApplicationWizard::class, 'targetAttribute' => ['user_application_wizard_id' => 'id']],
         ];
     }
@@ -110,7 +111,7 @@ class FormConfirmation extends \yii\db\ActiveRecord
      */
     public function getUser()
     {
-        return $this->hasOne(User::class, ['id' => 'user_id']);
+        return $this->hasOne(ImaUsers::class, ['id' => 'user_id']);
     }
 
     /**
