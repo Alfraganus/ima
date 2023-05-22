73e39a08c2a7c71782984fdf23ae9fea1feddee5
Gulomjon Sulaymonov <uzpsychologist@gmail.com>
2023-05-22 23:56:28 +0500

    advanced search ready

--- expert/modules/v1/services/ReadFormService.php
+++ expert/modules/v1/services/ReadFormService.php
@@ -3,10 +3,36 @@
 namespace expert\modules\v1\services;
 
 use common\models\ApplicationFormMedia;
+use common\models\forms\FormIndustryExample;
 use expert\models\ExpertFormMedia;
+use frontend\modules\api\service\FormReadService;
 
 class ReadFormService
 {
+
+    public function getForm55($user_application_id)
+    {
+        $industryExampleModel = FormIndustryExample::findOne(['user_application_id' => $user_application_id]);
+        $getMoreExamples = ApplicationFormMedia::findAll([
+            'application_id' => $user_application_id,
+            'form_id' => FormReadService::getFormIdByClass('common\models\forms\FormDocument'),
+        ]);
+        $result = [
+            'title' => $industryExampleModel->title,
+            'image' => $industryExampleModel->file,
+            'is_main' => $industryExampleModel->is_main,
+            'language' => $industryExampleModel->language,
+        ];
+        foreach ($getMoreExamples as $example) {
+            $result[] = [
+                'title' => null,
+                'image' => $example->file_path,
+                'is_main' => null,
+                'language' => null,
+            ];
+        }
+        return $result;
+    }
 
     public function getFormData($formModel, $postContent)
     {
@@ -15,20 +41,21 @@ class ReadFormService
             'module_id' => $postContent['module_id'],
             'tab_id' => $postContent['tab_id']
         ];
-         if (!$postContent['tab_id']) unset($queryFilter['tab_id']);
+        if (!$postContent['tab_id']) unset($queryFilter['tab_id']);
 
         return $formModel::findall($queryFilter);
     }
 
-    public static function getAttachments($user_id, $user_application_id,$module_id, $form_id)
+    public static function getAttachments($user_id, $user_application_id, $module_id, $form_id)
     {
         return ExpertFormMedia::find()->where([
-            'user_application_id'    => $user_application_id,
-            'user_id'           => $user_id,
-            'module_id'           => $module_id,
-            'form_id'           => $form_id,
-        ])->select(['id', 'file_name', 'file_path','file_extension'])->all();
+            'user_application_id' => $user_application_id,
+            'user_id' => $user_id,
+            'module_id' => $module_id,
+            'form_id' => $form_id,
+        ])->select(['id', 'file_name', 'file_path', 'file_extension'])->all();
     }
+
     public function getColumnByField($field = 210)
     {
         return [
