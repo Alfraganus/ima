b9dca5bd411536c177eaa66ae806b561d0da1aed
uzpsychologist@gmail.com <uzpsychologist@gmail.com>
2023-05-29 10:51:26 +0500

    user applciations done

--- frontend/modules/api/controllers/UserProfileController.php
+++ frontend/modules/api/controllers/UserProfileController.php
@@ -6,17 +6,18 @@ use common\models\UserApplications;
 use expert\modules\v1\controllers\DefaultController;
 use expert\modules\v1\services\ApplicationChatService;
 use expert\modules\v1\services\UserRoleService;
+use Yii;
 use yii\rest\Controller;
 use yii\web\UploadedFile;
 
 /**
  * Default controller for the `v1` module
  */
-class UserProfileController extends Controller
+class UserProfileController extends DefaultController
 {
-    public function actionGetUserApplications($user_application_id)
+    public function actionGetUserApplications($application_type_id)
     {
-        $userAplicaitons = UserApplications::findAll($user_application_id);
+        $userAplicaitons = UserApplications::findAll(['id'=>Yii::$app->user->id,'application_id'=>$application_type_id]);
         return $userAplicaitons;
     }
 }
