f6a1fcc3a88d082e3863c5ae79de42ba8b761688
Gulomjon Sulaymonov <uzpsychologist@gmail.com>
2023-05-29 23:24:54 +0500

    more and more changes

--- frontend/modules/api/controllers/UserProfileController.php
+++ frontend/modules/api/controllers/UserProfileController.php
@@ -2,8 +2,10 @@
 
 namespace frontend\modules\api\controllers;
 
+use common\models\Payments;
 use common\models\UserApplications;
 use expert\models\forms\ExpertForm10;
+use expert\models\forms\ExpertFormDecision;
 use expert\modules\v1\controllers\DefaultController;
 use expert\modules\v1\services\ApplicationChatService;
 use expert\modules\v1\services\UserRoleService;
@@ -21,11 +23,18 @@ class UserProfileController extends DefaultController
         $userAplicaitons = UserApplications::findAll(['id'=>Yii::$app->user->id,'application_id'=>$application_type_id]);
         return $userAplicaitons;
     }
+
     public function actionAdditionalInfoForProfile($user_application_id)
     {
-        $userForm10 = ExpertForm10::findOne(['user_application_id'=>$user_application_id]);
+        $query = ['user_application_id'=>$user_application_id];
+        $userForm10 = ExpertForm10::findOne($query);
+        $getInitialInvoice = Payments::findOne($query);
+        $getNewInvoice = ExpertFormDecision::findOne($query);
         return [
-            'form10'=>$userForm10
+            'new_invoice_for_licence'=>json_decode($getNewInvoice->extra_info),
+            'invoice_for_application_form'=>$getInitialInvoice->invoice_serial,
+            'form10'=>$userForm10,
+
         ];
     }
 }
