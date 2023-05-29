87295eb3de9d1ecf83a724bbf71627af78146bf8
uzpsychologist@gmail.com <uzpsychologist@gmail.com>
2023-05-29 16:26:11 +0500

    user applciations done

--- frontend/modules/api/controllers/UserProfileController.php
+++ frontend/modules/api/controllers/UserProfileController.php
@@ -3,6 +3,7 @@
 namespace frontend\modules\api\controllers;
 
 use common\models\UserApplications;
+use expert\models\forms\ExpertForm10;
 use expert\modules\v1\controllers\DefaultController;
 use expert\modules\v1\services\ApplicationChatService;
 use expert\modules\v1\services\UserRoleService;
@@ -19,5 +20,12 @@ class UserProfileController extends DefaultController
     {
         $userAplicaitons = UserApplications::findAll(['id'=>Yii::$app->user->id,'application_id'=>$application_type_id]);
         return $userAplicaitons;
+    }
+    public function actionAdditionalInfoForProfile($user_application_id)
+    {
+        $userForm10 = ExpertForm10::findOne(['user_application_id'=>$user_application_id]);
+        return [
+            'form10'=>$userForm10
+        ];
     }
 }
